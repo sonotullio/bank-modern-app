0907bac8da76f1294ae238093bd7ea126fc5e365
Tullio Forneris <tullio@Tullios-MBP.lan>
2023-06-03 15:15:39 +0200

    Done.

---
+++ bank_modern_app/src/components/Button.tsx
@@ -0,0 +1,7 @@
+const Button = ({ styles }: any) => {
+    return (
+        <button type="button" className={'py-4 px-6 bg-blue-gradient font-poppins font-medium text-primary outline-none ' + styles}>Get Started</button>
+    )
+}
+
+export default Button;
